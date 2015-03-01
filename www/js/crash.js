69df84d0a42b4e291acd892643195988d30fae42
Christopher Pattison <chris.pattison@live.com>
2015-02-28 20:20:08 -0600

    Added txt messages

--- www/js/crash.js
+++ www/js/crash.js
@@ -4,7 +4,7 @@ var crashDecel = 0;
 var isCrashing = false;
 const MIN_CRASH_ACCEL = 14; // > gravity + 1G braking
 const FREE_FALL = 0.5;
-const ACCEL_MONITOR_INTERVAL = 25;
+const ACCEL_MONITOR_INTERVAL = 10;
 const GRAVITY = 9.8;
 
 function registerWatcher() {
@@ -16,7 +16,7 @@ function registerWatcher() {
 					dropVel += GRAVITY * ACCEL_MONITOR_INTERVAL;
 				}else if(accelmag > MIN_CRASH_ACCEL) {
 					isCrashing = true;
-					crashDecel += magnitude(accel) * dt;
+					crashDecel += magnitude(accel) * ACCEL_MONITOR_INTERVAL;
 				}else if(isCrashing) {
 					isCrashing = false;
 					if(crashDecel > dropVel * 1.3) {
@@ -45,5 +45,15 @@ function Vector(x, y, z) {
 }
 
 function crashed(decel) {
-
+	var msg = "Fuck her right in the pussy";
+	alert("You are in a serious car accident and probably need medical attention. I would highly advise contacting emergency services.");
+	jQuery.post({
+		url:"https://api.twilio.com/2010-04-01/Accounts/AC4ef0f686e173833b4fc146530a2e3a0b/Messages.json",
+		data:{
+			From:"+19032744826",
+			To:"972800570",
+			Body:msg},
+		username:"AC4ef0f686e173833b4fc146530a2e3a0b",
+		password:"0b5e70297d9edd6677bed365291c68f7"
+	});
 }
