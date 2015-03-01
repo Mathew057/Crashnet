ca01c9d14536c0db2d89a3a319b8cee414563baf
Mathew057 <mathew0057@gmail.com>
2015-03-01 01:07:46 -0600

    ..

--- www/js/main.js
+++ www/js/main.js
@@ -2,14 +2,14 @@
  * Created by Mathew on 2/28/15.
  */
 window.onload=function waitonswipe() {
-    var current = document.getElementById('overview');
-    Hammer(current).on("swipeleft", function swipeleft() {
+    var current = document.getElementById('#overview');
+    $('#overview').Hammer.on("swipeleft", function swipeleft() {
         console.log('swipeleft');
-
+        $('#contact').animate({left: '100%'})
     });
     Hammer(current).on("swiperight", function swiperight() {
         console.log('swiperight');
-
+        $('#overview').animate({left: '100%'})
     });
     $('#overview').click(function click(){
         console.log('click')
